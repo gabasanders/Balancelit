dd271b243e8c7313dfad95b480eff569eb2e7505
Sanders <byd.gabriel.sanders.249002@gmail.com>
2026-04-22 13:14:46 -0300

    Update Add Info Page for Recurring

--- pages/2_Add_Info.py
+++ pages/2_Add_Info.py
@@ -46,7 +46,7 @@ def get_categories():
 
 categories = get_categories()
 
-entry_type = st.radio("Select entry type", ["Transactions", "Categories"], horizontal=True)
+entry_type = st.radio("Select entry type", ["Transactions", "Categories", "Recurring"], horizontal=True)
 
 with st.form("add_form"):
     if entry_type == "Transactions":
@@ -59,12 +59,20 @@ with st.form("add_form"):
             name_input = st.text_input("Name")
             value_input = st.number_input("Value", min_value=0.0, step=0.01)
             nfe_link_input = st.text_input("NFE-link")
-    else:
+    elif entry_type == "Categories":
         col1, col2 = st.columns(2)
         with col1:
             cat_name_input = st.text_input("Name")
         with col2:
             color_input = st.color_picker("Color", "#00f900")
+    elif entry_type == "Recurring":
+        col1, col2 = st.columns(2)
+        with col1:
+            rec_category_input = st.selectbox("Category", categories)
+            rec_type_input = st.selectbox("Type", ["expense", "income"])
+        with col2:
+            rec_name_input = st.text_input("Name")
+            rec_value_input = st.number_input("Value", min_value=0.0, step=0.01)
 
     add_btn = st.form_submit_button("Add to List")
 
@@ -79,11 +87,19 @@ if add_btn:
             "value": float(value_input),
             "nfe-link": nfe_link_input
         }
-    else:
+    elif entry_type == "Categories":
         entry = {
             "type_entry": "category",
             "name": cat_name_input,
             "color": color_input
+        }
+    elif entry_type == "Recurring":
+        entry = {
+            "type_entry": "recurring",
+            "name": rec_name_input,
+            "category": rec_category_input,
+            "type": rec_type_input,
+            "value": float(rec_value_input)
         }
     st.session_state.memory.append(entry)
     save_memory(st.session_state.memory)
@@ -97,6 +113,7 @@ if st.session_state.memory:
         try:
             txs = [e for e in st.session_state.memory if e.get("type_entry") == "transaction"]
             cats = [e for e in st.session_state.memory if e.get("type_entry") == "category"]
+            recs = [e for e in st.session_state.memory if e.get("type_entry") == "recurring"]
 
             if txs:
                 df = db.fetch_all_transactions()
@@ -130,6 +147,17 @@ if st.session_state.memory:
                     ])
                 db.insert_categories(rows_to_insert)
                 st.cache_data.clear()
+
+            if recs:
+                rows_to_insert = []
+                for entry in recs:
+                    rows_to_insert.append([
+                        entry["name"],
+                        entry["category"],
+                        entry["type"],
+                        entry["value"]
+                    ])
+                db.insert_recurring(rows_to_insert)
 
             clear_memory()
             st.session_state.memory = []
